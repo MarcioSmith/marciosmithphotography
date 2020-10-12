7437b00f86ffbab4a15f60dce278b7c49c024446
Marcio Smith <marciosmith@guest1s-MacBook-Pro.local>
2020-10-12 15:23:39 -0400

    Deploy gh-pages

--- _next/static/3bqgRz-0mD8i8yQOYq_ae/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-144344228af134e83bdf.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1078aee74b865cb69c62.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
